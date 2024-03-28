68abd537ce7668692692f7f4b97fe2321e1e0f84
philipmo <philipmo59@gmail.com>
2024-03-28 10:19:20 -0400

    finished

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,12 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        data_list = []
+        jsonfied_response = json.loads(self.get_response_body())
+        for response in jsonfied_response:
+            data_list.append(response)
+        return data_list
